47822877934c530902ec2de2de20fd1b05bce28e
fred <domaine75@gmail.com>
2023-07-17 12:26:08 +0200

    Add demo project

---
+++ AsyncHelloWorld/ConsoleAppTestDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AsyncHelloWorld;
+namespace ConsoleAppTestDemo
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Action<string> Display = Console.WriteLine;
+      HelloWorld.SayHelloAsync("johnny");
+      Display("Press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
